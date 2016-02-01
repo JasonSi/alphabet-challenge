47f8206afea09aa2f3075170551ea2267b51237b
JasonSi <529511542@qq.com>
2016-02-02 03:02:09 +0800

    测试结果用Canvas绘制，尽量避免通过修改页面作弊

--- public/app.js
+++ public/app.js
@@ -1,6 +1,20 @@
 var stats = document.getElementById("stats")
 stats.innerHTML = 'A'
 var flag = 65
+
+function createCanvas(score){
+  canvas = document.createElement("canvas")
+  canvas.width="600"
+  canvas.height="400"
+  canvas.innerHTML = ' '//不知为何一定要有这个不然没有关闭标签
+  stats.innerHTML=''
+  stats.appendChild(canvas)
+  ctx = canvas.getContext('2d')
+  ctx.fillStyle = "#436A3E"
+  ctx.font="150pt sans-serif"
+  ctx.fillText(score,50,250)
+}
+
 document.onkeydown = function(e){
   //屏蔽浏览器快捷键
   e.preventDefault()
@@ -30,7 +44,9 @@ document.onkeydown = function(e){
         score = (endTime - startTime)/1000.0
         if(score<10){
           //10秒之外不显示时间避免几万秒溢出容器，并给予嘲讽
-          stats.innerHTML = score
+          //成绩用Canvas绘制，尽力避免通过修改页面来作弊
+          //非要作弊可以直接改代码我也没什么办法
+          createCanvas(score)
         }
         else {
           stats.innerHTML = "Slow"
