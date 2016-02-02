e05d5e3906d834aa107858728a9b2b8ce98b2a74
JasonSi <529511542@qq.com>
2016-02-02 14:40:17 +0800

    修改播放音效的函数,代码简化; 修改按Space时的部分逻辑;

--- public/app.js
+++ public/app.js
@@ -16,9 +16,10 @@ function createCanvas(score){
 }
 
 function playAudio(status){
+  //传入起始播放时间，默认为0
+  startTime = arguments[1] ? arguments[1] : 0
   audio = document.getElementById(status.toLowerCase())
-  // audio.src="public/"+ status.toLowerCase() +'.wav' //效率太低
-  audio.currentTime = 0.2
+  audio.currentTime = startTime
   audio.loop = false
   audio.play()
 }
@@ -40,27 +41,29 @@ document.onkeydown = function(e){
       //输入正确的情况
       flag++
       //输入正确 清除声
-      playAudio('CLEAR')
+      playAudio('CLEAR',0.25)
 
       if (e.keyCode==65) {
-        startTime = Date.now()
+        gameStart = Date.now()
         stats.innerHTML = String.fromCharCode(flag)
       }
       else if(flag<91 && flag>65){
         stats.innerHTML = String.fromCharCode(flag)
       }
       else if(flag == 91){
-        endTime = Date.now()
-        score = (endTime - startTime)/1000.0
+        gameOver = Date.now()
+        score = (gameOver - gameStart)/1000.0
         if(score<10){
           //10秒之外不显示时间避免几万秒溢出容器，并给予嘲讽
           //成绩用Canvas绘制，尽力避免通过修改页面来作弊
           //非要作弊可以直接改代码我也没什么办法
           createCanvas(score)
+          //十秒内完成，胜利声
           playAudio('WIN')
         }
         else {
-          stats.innerHTML = "Slow"
+          stats.innerHTML = "LOSE"
+          // 十秒外完成，失败声
           playAudio('LOSE')
         }
       }
@@ -68,13 +71,16 @@ document.onkeydown = function(e){
     }
     else{
       //警告声 输入错误
-      playAudio('WRONG')
+      playAudio('WRONG',0.2)
     }
 
   }
 
   else{
     //非字母、空格按键 警告声
-    playAudio('ERROR')
+    if (e.keyCode!=32){
+      playAudio('WRONG',0.1)
+      // playAudio('ERROR',0.1)
+    }
   }
 }
